ca7439a6c4e93502e4e884be2060a3413afb1e1c
D-mitry12344 <bityukov02@list.ru>
2023-03-23 21:14:37 +0300

    mobile_version

--- index.js
+++ index.js
@@ -2,6 +2,95 @@
   console.log('Привет '+ this.name + '. Как дела?');
 };
 */
+let btnRetNews = document.querySelector('.news__block-nav__return');
+let btnNextNews = document.querySelector('.news__block-nav__next');
+const masNewsblock = document.querySelectorAll('.news_items-block')
+const masNews= document.querySelectorAll('#news')
+
+
+btnNextNews.addEventListener('click', function(){
+	/*if(window.innerWidth > 700){
+		for(let i = 0 ; i< masNews.length ; i++){
+		let constNumb = masNews.length;
+		 	if(masNews[i].classList[1] !== 'news_items-disabled'){
+				masNews[i].classList.add('news_items-disabled');
+				let e = i + 1;
+				if(e === constNumb){
+					let e = 0;
+					let i = 0;
+					masNews[e].classList.remove('news_items-disabled');
+					break
+				}
+				masNews[e].classList.remove('news_items-disabled');
+				break
+			}
+		}
+	}	*/
+	if(window.innerWidth < 700){
+		masNewsblock.forEach(function(item){
+			if(item !== 'news_items-block-disabled'){
+				let e = 0;
+				for(let i = 0 ; i < masNewsblock.length ; i++){
+					let constNumb = masNewsblock.length;
+
+					if(item === masNewsblock[i]){
+						masNewsblock[i].classList.add('news_items-block-disabled');
+
+						e++;
+						/*if(e === constNumb){
+							let e = 0;
+							let i = 0;
+							masNewsblock[e].classList.remove('news_items-block-disabled');
+							break
+						};*/
+						masNewsblock[e].classList.remove('news_items-block-disabled');
+						return e;
+						
+					}
+				}
+			}
+		})
+
+		/*for(let i = 0 ; i < masNewsblock.length ; i++){
+			//let constNumb = masNewsblock.length;
+		 	if(masNewsblock[i].classList[1] !== 'news_items-block-disabled' || masNewsblock[i].classList[2] !== 'news_items-block-disabled'  
+		 		 || masNewsblock[i].classList[0] !== 'news_items-block-disabled'  || masNewsblock[i].classList[4] !== 'news_items-block-disabled'){
+				masNewsblock[i].classList.add('news_items-block-disabled');
+				let e = i + 1;
+				if(e === constNumb){
+					let e = 0;
+					let i = 0;
+					masNewsblock[e].classList.remove('news_items-block-disabled');
+					break
+				};
+				console.log(masNewsblock[i].classList[1] !== 'news_items-block-disabled')
+
+				masNewsblock[e].classList.remove('news_items-block-disabled');
+				
+			}
+		}*/
+	}
+})
+
+
+btnRetNews.addEventListener('click', function(){
+	for(let i = 0 ; i< masNews.length ; i++){
+		let constNumb = masNews.length;
+	 	if(masNews[i].classList[1] !== 'news_items-disabled'){
+			masNews[i].classList.add('news_items-disabled');
+			let e = i - 1;
+			if(e === -1){
+				let e = masNews.length-1;
+				let i = masNews.length-1;
+				masNews[e].classList.remove('news_items-disabled');
+				break
+			}
+			masNews[e].classList.remove('news_items-disabled');
+			break
+		}
+	}
+})
+
 
 let bannerFirst = document.querySelector(".banner_header__myself-first")
 let bannerSecond = document.querySelector(".banner_header__myself-second")
